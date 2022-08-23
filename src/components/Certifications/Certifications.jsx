bb2f43d3ed2e01c0b02f0172243944b88536a666
LionelOrbe <lionel05@gmail.com>
2022-08-23 08:55:59 -0300

    Certification fixes

--- src/components/Certifications/Certifications.jsx
+++ src/components/Certifications/Certifications.jsx
@@ -14,14 +14,21 @@ export default function Certifications() {
       </div>
       <div>
         <p>Certified PRO Network</p>
+        <a href='https://www.proz.com/translator/740492'  style={{ textDecoration: "none" }} target="_blank" rel="noreferrer">
         <img src={pro} alt='PRO' className={style.pro}/>
+        </a>
       </div>
       <div>
         <p>AATI Member</p>
+        <a href='https://www.aati.org.ar'  style={{ textDecoration: "none" }} target="_blank" rel="noreferrer">
         <img src={aati} alt='AATI' className={style.aati}/>
+        </a>
       </div>
-      <div>
+      <div className={style.traductor}>
+        <a href='http://newstart.edu.ar/newstart_CI/home/campus'  style={{ textDecoration: "none" }} target="_blank" rel="noreferrer">
         <p>Traductor literario y técnico científico de inglés</p>
+        <p>Instituto Superior "New Start"</p>
+        </a>
       </div>
     </div>
   )
